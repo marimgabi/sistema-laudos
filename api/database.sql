f023a6d23a3afb3ebe680612b87224385a95fe12
marimgabi <gabimarim06@gmail.com>
2025-04-15 21:59:36 -0300

    Adição de estrutura do banco e dockerfile

--- api/database.sql
+++ api/database.sql
@@ -1,136 +1,142 @@
--- TABELA role
-CREATE TABLE role (
-    id SERIAL PRIMARY KEY,
-    role VARCHAR(100) NOT NULL,
-    descricao TEXT
+CREATE DATABASE logic WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'en_US.utf8';
+
+CREATE SCHEMA public;
+
+CREATE TABLE public.conselho (
+    id integer NOT NULL,
+    tipo character varying(255) NOT NULL,
+    numero character varying(255) NOT NULL,
+    id_estado integer,
+    data_inclusao timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    data_alteracao timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    status character varying(255) NOT NULL,
+    CONSTRAINT conselho_status_check CHECK (((status)::bpchar = ANY (ARRAY['A'::bpchar, 'I'::bpchar])))
 );
 
--- TABELA user
-CREATE TABLE "user" (
-    id SERIAL PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    username VARCHAR(100) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    data_inclusao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    data_alteracao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    status CHAR(1) CHECK (status IN ('A', 'I')) NOT NULL
+CREATE SEQUENCE public.conselho_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE TABLE public.estado (
+    id integer NOT NULL,
+    nome character varying(255),
+    sigla character varying(2),
+    data_inclusao timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    data_alteracao timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    status character varying(255) NOT NULL,
+    CONSTRAINT estado_status_check CHECK (((status)::bpchar = ANY (ARRAY['A'::bpchar, 'I'::bpchar])))
 );
 
--- TABELA user_role
-CREATE TABLE user_role (
-    user_id INT,
-    role_id INT,
-    data_inclusao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    data_alteracao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    status CHAR(1) CHECK (status IN ('A', 'I')) NOT NULL,
-    PRIMARY KEY (user_id, role_id),
-    CONSTRAINT fk_userrole_user FOREIGN KEY (user_id) REFERENCES "user"(id),
-    CONSTRAINT fk_userrole_role FOREIGN KEY (role_id) REFERENCES role(id)
+CREATE SEQUENCE public.estado_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE TABLE public.laudo (
+    id integer NOT NULL,
+    data_laudo timestamp(6) without time zone,
+    nome_paciente character varying(255),
+    sexo character varying(255),
+    data_nascimento timestamp(6) without time zone,
+    id_medico_solicitante integer,
+    id_medico_executante integer,
+    conteudo text,
+    data_inclusao timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    data_alteracao timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    status character varying(255) NOT NULL,
+    CONSTRAINT laudo_sexo_check CHECK (((sexo)::bpchar = ANY (ARRAY['M'::bpchar, 'F'::bpchar, 'O'::bpchar]))),
+    CONSTRAINT laudo_status_check CHECK (((status)::bpchar = ANY (ARRAY['A'::bpchar, 'I'::bpchar])))
 );
 
--- TABELA estado
-CREATE TABLE estado (
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR(100),
-    sigla CHAR(2),
-    data_inclusao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    data_alteracao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    status CHAR(1) CHECK (status IN ('A', 'I')) NOT NULL
+CREATE SEQUENCE public.laudo_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE TABLE public.medico (
+    id integer NOT NULL,
+    nome character varying(255) NOT NULL,
+    tipo character varying(255),
+    id_user integer,
+    id_conselho integer,
+    data_inclusao timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    data_alteracao timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    status character varying(255) NOT NULL,
+    CONSTRAINT medico_status_check CHECK (((status)::bpchar = ANY (ARRAY['A'::bpchar, 'I'::bpchar])))
 );
 
--- TABELA conselho
-CREATE TABLE conselho (
-    id SERIAL PRIMARY KEY,
-    tipo VARCHAR(50) NOT NULL,
-    numero VARCHAR(50) NOT NULL,
-    id_estado INT,
-    data_inclusao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    data_alteracao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    status CHAR(1) CHECK (status IN ('A', 'I')) NOT NULL,
-    CONSTRAINT fk_conselho_estado FOREIGN KEY (id_estado) REFERENCES estado(id)
+CREATE SEQUENCE public.medico_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE TABLE public.medico_template (
+    id_medico integer NOT NULL,
+    id_template integer NOT NULL
 );
 
--- TABELA medico
-CREATE TABLE medico (
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
-    tipo VARCHAR(50),
-    id_user INT,
-    id_conselho INT,
-    data_inclusao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    data_alteracao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    status CHAR(1) CHECK (status IN ('A', 'I')) NOT NULL,
-    CONSTRAINT fk_medico_user FOREIGN KEY (id_user) REFERENCES "user"(id),
-    CONSTRAINT fk_medico_conselho FOREIGN KEY (id_conselho) REFERENCES conselho(id)
+CREATE TABLE public.role (
+    id integer NOT NULL,
+    role character varying(255) NOT NULL,
+    descricao character varying(255)
 );
 
--- TABELA template
-CREATE TABLE template (
-    id SERIAL PRIMARY KEY,
-    descricao VARCHAR(255),
-    conteudo TEXT,
-    tipo VARCHAR(50),
-    data_inclusao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    data_alteracao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    status CHAR(1) CHECK (status IN ('A', 'I')) NOT NULL
+CREATE SEQUENCE public.role_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE TABLE public.template (
+    id integer NOT NULL,
+    descricao character varying(255),
+    conteudo text,
+    tipo character varying(255),
+    data_inclusao timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    data_alteracao timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    status character varying(255) NOT NULL,
+    CONSTRAINT template_status_check CHECK (((status)::bpchar = ANY (ARRAY['A'::bpchar, 'I'::bpchar])))
 );
 
--- TABELA medico_template
-CREATE TABLE medico_template (
-    id_medico INT,
-    id_template INT,
-    data_inclusao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    data_alteracao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    status CHAR(1) CHECK (status IN ('A', 'I')) NOT NULL,
-    PRIMARY KEY (id_medico, id_template),
-    CONSTRAINT fk_medtemp_medico FOREIGN KEY (id_medico) REFERENCES medico(id),
-    CONSTRAINT fk_medtemp_template FOREIGN KEY (id_template) REFERENCES template(id)
+CREATE SEQUENCE public.template_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE TABLE public."user" (
+    id integer NOT NULL,
+    email character varying(255) NOT NULL,
+    username character varying(100) NOT NULL,
+    password character varying(255) NOT NULL,
+    data_inclusao timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    data_alteracao timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    status character(1) NOT NULL,
+    id_role integer,
+    CONSTRAINT user_status_check CHECK ((status = ANY (ARRAY['A'::bpchar, 'I'::bpchar])))
 );
 
--- TABELA laudo
-CREATE TABLE laudo (
-    id SERIAL PRIMARY KEY,
-    data_laudo DATE,
-    nome_paciente VARCHAR(255),
-    sexo CHAR(1) CHECK (sexo IN ('M', 'F', 'O')),
-    data_nascimento DATE,
-    id_medico_solicitante INT,
-    id_medico_executante INT,
-    conteudo TEXT,
-    data_inclusao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    data_alteracao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    status CHAR(1) CHECK (status IN ('A', 'I')) NOT NULL,
-    CONSTRAINT fk_laudo_med_solic FOREIGN KEY (id_medico_solicitante) REFERENCES medico(id),
-    CONSTRAINT fk_laudo_med_exec FOREIGN KEY (id_medico_executante) REFERENCES medico(id)
-);
-
--- Insert com os estados
-
-INSERT INTO estado (nome, sigla, data_inclusao, data_alteracao, status) VALUES
-('Acre', 'AC', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'A'),
-('Alagoas', 'AL', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'A'),
-('Amapá', 'AP', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'A'),
-('Amazonas', 'AM', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'A'),
-('Bahia', 'BA', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'A'),
-('Ceará', 'CE', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'A'),
-('Distrito Federal', 'DF', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'A'),
-('Espírito Santo', 'ES', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'A'),
-('Goiás', 'GO', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'A'),
-('Maranhão', 'MA', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'A'),
-('Mato Grosso', 'MT', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'A'),
-('Mato Grosso do Sul', 'MS', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'A'),
-('Minas Gerais', 'MG', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'A'),
-('Pará', 'PA', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'A'),
-('Paraíba', 'PB', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'A'),
-('Paraná', 'PR', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'A'),
-('Pernambuco', 'PE', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'A'),
-('Piauí', 'PI', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'A'),
-('Rio de Janeiro', 'RJ', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'A'),
-('Rio Grande do Norte', 'RN', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'A'),
-('Rio Grande do Sul', 'RS', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'A'),
-('Rondônia', 'RO', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'A'),
-('Roraima', 'RR', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'A'),
-('Santa Catarina', 'SC', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'A'),
-('São Paulo', 'SP', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'A'),
-('Sergipe', 'SE', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'A'),
-('Tocantins', 'TO', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'A');
+CREATE SEQUENCE public.user_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
